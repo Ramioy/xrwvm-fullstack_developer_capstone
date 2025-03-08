9929a437b472dc1c5ad5ab48db075e1ea605985c
Yoimar <yoimar.mb@outlook.com>
2025-03-08 09:28:27 -0500

    carmodels

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -14,11 +14,23 @@ import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
+from .models import CarMake, CarModel
 
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
+@csrf_exempt
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
 
 # Create your views here.
 
@@ -39,6 +51,7 @@ def login_user(request):
     return JsonResponse(data)
 
 # Create a `logout_request` view to handle sign out request
+@csrf_exempt
 def logout_request(request):
     logout(request)
     data = {"userName":""}
